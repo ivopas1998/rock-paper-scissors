8f9596c379110dcdf7644083a8b2c18c886e1941
Ivo Pas <ivopas1998@gmail.com>
2021-03-28 18:20:42 +0100

    changed `fucntion game`, now has multiple arguments in each statement and changed some text to make it more readable

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,5 +1,5 @@
 //welcome user to the game and let user select amount of rounds to play
-let rounds = prompt('Welcome to rock paper scissors! How many rounds would you like to play? You can pick from 3, 5, 7 or 10 rounds!')
+let rounds = prompt('Welcome to rock paper scissors! \nHow many rounds would you like to play? \nYou can pick from 3, 5, 7 or 10 rounds!')
 rounds = Number(rounds);
 //tell the player to pick a valid amount of rounds if they haven't already
 //if it's an unvalid amount of rounds, make sure the player selects the right amount
@@ -9,8 +9,8 @@ while(validRoundsAmount == false) {
         alert('You chose to play ' + rounds + ' rounds, good luck!');
         validRoundsAmount = true;
     } else {
-    rounds = prompt('That\'s an invalid amount of rounds! Please pick 3, 5, 7 or 10 rounds!');
-    rounds = Number(rounds);
+        rounds = prompt('That\'s an invalid amount of rounds! \nPlease pick from 3, 5, 7 or 10 rounds!');
+        rounds = Number(rounds);
     }
 }
 //keep track of the player and computer score
@@ -30,14 +30,14 @@ while(playerScore !== rounds && computerScore !== rounds) {
 //if they choose anything else, let them know the choice is invalid
 //make sure that the players choice is case insensitive
     function playerPlay() {
-        let playerChoices = prompt('Choose rock, paper or scissors');
+        let playerChoices = prompt('Choose rock, paper or scissors.');
         playerChoices = playerChoices.toLowerCase();
         let validPlayerChoice = false;
         while(validPlayerChoice == false) {
             if(playerChoices === 'rock' || playerChoices === 'paper'|| playerChoices === 'scissors') {
                 validPlayerChoice = true;
-            } else{
-                playerChoices = prompt('That is an invalid choice! Please pick from either rock, paper or scissors!');
+            } else {
+                playerChoices = prompt('That\'s an invalid choice! \nPlease pick from either rock, paper or scissors!');
                 playerChoices = playerChoices.toLowerCase();
             }
         }
@@ -46,29 +46,22 @@ while(playerScore !== rounds && computerScore !== rounds) {
     const playerSelection = playerPlay();
     const computerSelection = computerPlay();
 //make a function that outputs the winner between the player and computer
-    function game(playerSelection, computerSelection) {
-        if(computerSelection === 'scissors' && playerSelection === 'rock') {
-            playerScore ++; 
-            alert('You chose rock, computer chose scissors. Rock wins from scissors! You now have ' + playerScore + ' point(s)');
-        } else if(computerSelection === 'rock' && playerSelection === 'paper') {
-            playerScore ++;
-            alert('You chose paper, computer chose rock. Paper wins from rock! You now have ' + playerScore + ' point(s)');
-        } else if(computerSelection === 'paper' && playerSelection === 'scissors') {
-            playerScore ++;
-            alert('You chose scissors, computer chose paper. Scissors wins from paper! You now have ' + playerScore + ' point(s)');
-        } else if(computerSelection === 'rock' && playerSelection === 'scissors') {
-            computerScore ++;
-            alert('You chose scissors, computer chose rock. Rock wins from scissors! The computer now has ' + computerScore + ' point(s)');
-        } else if(computerSelection === 'paper' && playerSelection === 'rock') {
-            computerScore ++;
-            alert('You chose rock, computer chose paper. Paper wins from rock! The computer now has ' + computerScore + ' point(s)');
-        } else if(computerSelection === 'scissors' && playerSelection === 'paper') {
-            computerScore ++;
-            alert('You chose paper, computer chose scissors. Scissors wins from paper! The computer now has ' + computerScore + ' point(s)');
-        } else {
-            alert('The computer chose the same as you, it\'s a tie! The score is: Player: ' + playerScore + ' Computer: ' + computerScore);
-        }
+function game(playerSelection, computerSelection) {
+    if((computerSelection === 'scissors' && playerSelection === 'rock') 
+    || (computerSelection === 'rock' && playerSelection === 'paper') 
+    || (computerSelection === 'paper' && playerSelection === 'scissors')) {
+        playerScore ++; 
+        alert(`${playerSelection} VS ${computerSelection}. You win! \nYou now have ${playerScore} point(s).`);
+    
+    } else if((computerSelection === 'rock' && playerSelection === 'scissors')
+    || (computerSelection === 'paper' && playerSelection === 'rock')
+    || (computerSelection === 'scissors' && playerSelection === 'paper')) {
+        computerScore++;
+        alert(`${playerSelection} VS ${computerSelection}. You lose! \nComputer now has ${computerScore} point(s).`);
+    } else {
+        alert(`The computer chose the same, it's a tie! \nThe score is: You: ${playerScore} Computer: ${computerScore}.`);
     }
+}
     game(playerSelection, computerSelection);
 }
 //if player has points equivalent to the number of rounds selected the player wins
@@ -76,7 +69,7 @@ while(playerScore !== rounds && computerScore !== rounds) {
 if(playerScore === rounds) {
     alert('Congratulations, you\'ve won the game!');
 } else {
-    alert('Unfortunately the computer has won, better luck next time!');
+    alert('Unfortunately the computer has won. \nBetter luck next time!');
 }
 //restarts the game automatically after the game is over
 location.reload();
